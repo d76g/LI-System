d5e4c1ca6c7cfba3b8d398c94963ee1c0a057ed3
d76g <basharalshaibani@gmail.com>
2022-01-11 06:13:47 +0800

    v3

--- routes/web.php
+++ routes/web.php
@@ -54,7 +54,7 @@ Route::get('/document', [DocumentController::class, 'viewdoc'])->name('docPage')
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     $students = DB::table('students')->skip(0)->take(PHP_INT_MAX)->get();
     $location = DB::table('students')
-        ->select(DB::raw('count(students.Negeri) as NumberOfStudents, students.Negeri'))
+        ->select(DB::raw('count(Negeri) as NumberOfStudents, Negeri'))
         ->groupBy('Negeri')
         ->orderByDesc('NumberOfStudents')
         ->get();
